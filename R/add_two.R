69a064ab232dd72416654a30a7ecf8a3219270d8
Mehmet Imga <imgamehmet@gmail.com>
2026-01-07 19:38:45 -0800

    first commit

---
+++ R/add_two.R
@@ -0,0 +1,13 @@
+#' Add two numbers
+#'
+#' @param x Numeric value.
+#' @param y Numeric value.
+#'
+#' @return Sum of `x` and `y`.
+#' @examples
+#' add_two(1, 2)
+#' @export
+add_two <- function(x, y) {
+  x + y
+}
+
